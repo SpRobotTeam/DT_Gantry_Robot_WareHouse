09492ffde2d7a652d10725545b0bd9bbe82817fe
maroon <lmrlar98@gmail.com>
2024-04-02 15:13:38 +0900

    240402 db_wrapper 수정 중

--- API/odoo_api_wrapper.py
+++ API/odoo_api_wrapper.py
@@ -3,6 +3,60 @@ import xmlrpc.client
 
 
 class odoo_xmlrpc():
+    templet_format = {
+  
+        # 'name': 'test_05',
+        'detailed_type': 'product',
+        'type': 'product',
+        'responsible_id': [2, 'Mitchell Admin'],
+        'volume': 0.0,
+        'volume_uom_name': 'm³',
+        'weight': 0.0,
+        'weight_uom_name': 'kg',
+        
+            }
+    
+    loaction_format = {
+        
+        # 'child_ids': [],
+        # 'child_internal_location_ids': [49],
+        'comment': False,
+        'company_id': [3, 'DT'],
+        'complete_name': 'loc_format',
+        'create_uid': [2, 'Mitchell Admin'],
+        'cyclic_inventory_frequency': 0,
+        # 'id': 49,
+        # 'incoming_move_line_ids': [],
+        # 'last_inventory_date': False,
+        # 'location_id': False,
+        'name': 'loc_format',
+        # 'net_weight': 0.0,
+        'next_inventory_date': False,
+        # 'outgoing_move_line_ids': [],
+        # 'parent_path': '49/',
+        'posx': 0,
+        'posy': 0,
+        'posz': 0,
+        'putaway_rule_ids': [],
+        'quant_ids': [],
+        'removal_strategy_id': False,
+        # 'replenish_location': False,
+        # 'return_location': False,
+        # 'scrap_location': False,
+        # 'storage_category_id': False,
+        # 'usage': 'internal',
+        # 'warehouse_id': False,
+        # 'warehouse_view_ids': [],
+        'write_uid': [2, 'Mitchell Admin']
+        }
+    
+
+    
+
+
+
+
+
     def __init__(self, url=None, db=None, username=None, password=None, api_key=None):
     
         self.__url      = url if url else 'http://127.0.0.1:8069'
@@ -45,7 +99,7 @@ class odoo_xmlrpc():
         '''
         # data = 
         self.__models.execute_kw(self.__db, self.__uid, self.__key, obj, 'create', args)
-        # return data
+        return None
 
 
 
@@ -118,28 +172,100 @@ class odoo_xmlrpc():
         `args` : 데이터 속성(내용)
 
         ex)
-        search_data('res.partner', [[['id', '=', id], ]],)
+        delete_data('res.partner', [[['id', '=', id], ]],)
         '''
         # data = 
         self.__models.execute_kw(self.__db, self.__uid, self.__key, obj, 'unlink', args)
-        # return data
+        return None
 
 
+    def db_wrapper(self, obj:str, method:str, args = None, kw = None):
+        data = self.list_up_record_filds(obj=obj, args=args, kw=kw)
+        if method == 'read' or args in data:
+            pass
+        
+        else:
+            if method == 'create' or args not in data:
+                pass
+
+            else:
+                for _ in args:
+                    for i in _:
+                        if type(i) == dict:
+                            method = 'write'
+                        else:
+                            method = 'unlink'
+                if method == 'write':
+                    pass
+
+                elif method == 'unlink':
+                    pass
+
+
+    # def edit_company(method:str, args=None):
+    #     obj = 'res.company'
+        
+    #     return_val = ''
+    #     return return_val
+
+    # def edit_warehouse(method:str, args=None):
+    #     obj = 'stock.warehouse'
+        
+    #     return_val = ''
+    #     return return_val
+
+    # def edit_location(method:str, args=None):
+    #     obj = 'stock_location'
+        
+    #     return_val = ''
+    #     return return_val
+
+    # def edit_templet(method:str, args=None):
+    #     obj = 'product.template'
+        
+    #     return_val = ''
+    #     return return_val
+
+    # def edit_product(method:str, args=None):
+    #     obj = 'product.product'
+        
+    #     return_val = ''
+    #     return return_val
+
+    # def edit_quant(method:str, args=None):
+    #     obj = 'stock.quantity'
+        
+    #     return_val = ''
+    #     return return_val
+
+
+    # def edit_lot(method:str, args=None):
+    #     obj = 'stock.lot'
+        
+    #     return_val = ''
+    #     return return_val
 
 if __name__ == '__main__':
     test = odoo_xmlrpc()
     # test.create_data(obj='product.product', args=[])
     test.login()
     import pprint
-    # test.create_data(obj='product.template',
-    #                  args=[{'name':'test03', }]
-    #                  )
 
-    data = test.search_data(obj='product.template', 
-                            # args=[[['type', '=', 'product']]],
-                            args=[[['write_uid', '=', 2]]], 
-                            kw={'fields':['name', 'id', ]}
-                            )
+    # ['stock_quant']
+    table_name = 'stock.picking' # 'stock.location'
+    # args=[{'name' : 'loc_format', }]
+    inst_name = table_name.split('.')[-1]+'_template'
+    test.create_data(obj=table_name, 
+                    # args=[[['type', '=', 'product']]],
+                    args=[{'name' : inst_name, }], 
+                    )
+    
+    data = test.search_data(obj=table_name, 
+                    # args=[[['type', '=', 'product']]],
+                    args=[[['name', '=', inst_name]]],
+                    # kw={'fields':['name', 'id', ]}
+                    kw={}
+                    )
     sorted_data = sorted(data, key=lambda x:(x['name']))
     pprint.pprint(sorted_data)
     print([x for x in sorted_data if ' ' in x['name']])
