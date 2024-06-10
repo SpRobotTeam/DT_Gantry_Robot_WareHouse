362865e7ab911333abe525c8abd6d1d7584f067f
maroon <lmrlar98@gmail.com>
2024-06-10 16:17:18 +0900

    240610 - 로그 기능 추가

--- SIM/RoboDK/plc_motion006.py
+++ SIM/RoboDK/plc_motion006.py
@@ -23,7 +23,8 @@ pathlib.Path("./logs/plc_motion006.log").touch
 log_file_handler = logging.handlers.RotatingFileHandler(filename="./logs/plc_motion006.log", 
                                     mode="a",
                                     backupCount= 3,
-                                    maxBytes= 1024*1024*512
+                                    maxBytes= 1024*1024*512,
+                                    encoding='utf-8'
                                     )
 log_formater = logging.Formatter("{asctime} {levelname} {filename}>{funcName} {message}", style='{')
 log_file_handler.setFormatter(log_formater)
--- main.py
+++ main.py
@@ -23,7 +23,8 @@ pathlib.Path("./logs/main.log").touch
 log_file_handler = logging.handlers.RotatingFileHandler(filename="./logs/main.log", 
                                     mode="a",
                                     backupCount= 3,
-                                    maxBytes= 1024*1024*512
+                                    maxBytes= 1024*1024*512,
+                                    encoding='utf-8'
                                     )
 log_formater = logging.Formatter("{asctime} {levelname} {filename}>{funcName} {message}", style='{')
 log_file_handler.setFormatter(log_formater)
